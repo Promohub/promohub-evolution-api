d4c48eefee50ff7fb55e48ba00c4883e74d3f476
vguille <viniciusguilherme94@hotmail.com>
2024-10-10 20:26:29 -0300

    Ajustes script deploy local

--- deploy-docker-local.sh
+++ deploy-docker-local.sh
@@ -2,4 +2,4 @@
 sudo docker build -t promohub/evolution-api .
 
 #Run local container
-sudo docker run -p 8080:8080 --net='evolution-net' -e CACHE_REDIS_URI='redis://redis:6379/6' -e DATABASE_PROVIDER='postgresql' -e DATABASE_CONNECTION_URI='postgresql://postgres:root@postgres:5432/evolution?schema=public' promohub/evolution-api
+sudo docker run -p 8080:8080 --name evolution-api --net='evolution-net' -e CACHE_REDIS_URI='redis://redis:6379/6' -e DATABASE_PROVIDER='postgresql' -e DATABASE_CONNECTION_URI='postgresql://postgres:root@postgres:5432/evolution?schema=public' promohub/evolution-api
